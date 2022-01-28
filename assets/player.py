1b4b13f9d850f4129260a0d651c2571707001173
Plompi <37980624+Plompi@users.noreply.github.com>
2022-01-28 14:30:56 +0100

    Update and rename Player.py to player.py

--- assets/player.py
+++ assets/player.py
@@ -1,10 +1,12 @@
 import pygame
 
 class Spieler:
-    def __init__(self,position:tuple,color:tuple,win):
-        self.__x,self.__y = position[0],position[1]-50
+    def __init__(self,position:tuple,win,scheme):
+        self.__position = position
+        self.__x,self.__y = self.__position[0],self.__position[1]-50
         self.__win = win
-        self.__color = color
+        self.__scheme = scheme
+        self.__color = self.__scheme.loadactivecolor()
         self.__speed = 4
         self.__player = pygame.Rect(self.__x, self.__y, 5, 100)
         self.__Input = (False,False) #top,bottom
@@ -16,7 +18,7 @@ class Spieler:
         if self.__Input[1] and not self.__Input[0] and self.__y + self.__player.height + self.__speed <= self.__win.get_height(): #Höhe des Fensters
             self.__y += self.__speed
         self.__player = pygame.Rect(self.__x,self.__y, 5, 100)
-        pygame.draw.rect(self.__win, self.__color, self.__player,0,20)
+        pygame.draw.rect(self.__win, self.__color[3], self.__player,0,20)
 
     def setInput(self,newInput:tuple):
         self.__Input= newInput
@@ -29,3 +31,8 @@ class Spieler:
 
     def getPoints(self):
         return self.__Points
+
+    def restart(self):
+        self.__Points = 0
+        self.__x,self.__y = self.__position[0],self.__position[1]-50
+        pygame.Rect(self.__x, self.__y, 5, 100)
